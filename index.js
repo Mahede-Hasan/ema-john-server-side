cb0b919faf0f6e402b767adb7613457e38d0528f
Mahede-Hasan <mahedehasan.cd91@gmail.com>
2022-09-21 17:27:01 -0700

    ema-john server side added

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ihbrvor.mongodb.net/?retryWrites=true&w=majority`
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('emaJohn').collection('product');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+        app.get('/productCount', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const count = await cursor.count();
+            res.send({ count })
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
+
+
+app.get('/', (req, res) => {
+    res.send('hello from express.js')
+})
+
+app.listen(port, () => {
+    console.log('server running', port)
+})
