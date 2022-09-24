1ce61ac2f47803468d515e2bab03b0e8cef9fc83
Mahede-Hasan <mahedehasan.cd91@gmail.com>
2022-09-24 14:22:58 -0700

    updated heroku

--- index.js
+++ index.js
@@ -19,20 +19,12 @@ async function run() {
         const productCollection = client.db('emaJohn').collection('product');
 
         app.get('/product', async (req, res) => {
-<<<<<<< HEAD
             console.log('query', req.query);
-=======
->>>>>>> aa5c44d5ea98c612319493b87b171eaa59050484
             const page = parseInt(req.query.page);
             const size = parseInt(req.query.size);
 
             const query = {};
-<<<<<<< HEAD
             const cursor = productCollection.find(query);
-=======
-            const cursor = productCollection.find
-                (query);
->>>>>>> aa5c44d5ea98c612319493b87b171eaa59050484
             let products;
             if (page || size) {
                 products = await cursor.skip(page * size).limit(size).toArray();
@@ -46,21 +38,7 @@ async function run() {
 
         app.get('/productCount', async (req, res) => {
             const count = await productCollection.estimatedDocumentCount();
-<<<<<<< HEAD
-            res.send({ count });
-        });
-
-        app.post('/productByKeys', async (req, res) => {
-            const keys = req.body;
-            const ids = keys.map(id => ObjectId(id));
-            const query = { _id: { $in: ids } }
-            const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
-            console.log(keys);
-            res.send(products);
-=======
             res.send({ count })
->>>>>>> aa5c44d5ea98c612319493b87b171eaa59050484
         })
     }
     finally {
